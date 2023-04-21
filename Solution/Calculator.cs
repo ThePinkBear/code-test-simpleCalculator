ce6bfc336522dbe1599eca2d932a6f8ca331d148
ThePinkBear <bjorn.noctiluca@outlook.com>
2023-04-21 11:20:57 +0200

    Implemented main Calculator logic

--- Solution/Calculator.cs
+++ Solution/Calculator.cs
@@ -1,11 +1,32 @@
-using System.Linq;
+namespace SectraCalculator;
+
+public class Calculator
+{
+  private int _queuedCalculations;
+  public int PendingCalculations() => _queuedCalculations;
+
+  public Calculator AddCalculation(CalculationCommand calCom)
+  {
+    return calCom.operation switch
+    {
+      Operation.add
+      => new(){_queuedCalculations = _queuedCalculations + calCom.values.Sum()},
+      Operation.subtract
+      => new(){_queuedCalculations = _queuedCalculations - calCom.values.Sum()},
+      Operation.multiply
+      => new(){_queuedCalculations = _queuedCalculations * calCom.values.Sum()},
+      _
+      => throw new ArgumentException("Unsupported operation")
+    };
+  }
+}
 
 public enum Operation {add, subtract, multiply}
 
 public class CalculationCommand
 {
   public Operation operation { get; private set; }
-  public int[]? values { get; private set; }
+  public int[] values { get; private set; }
 
   private CalculationCommand(){}
   public static class CalculationCommandFactory
